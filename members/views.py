8d3011c2ca0f2a11498afd045b5ea58e1e719c1a
zachariahbioto3-sketch <zachariahbioto3@gmail.com>
2026-07-17 10:19:46 +0300

    fix: DEFAULT_AUTO_FIELD, email backend env config, gallery urls, re-enable signup

--- members/views.py
+++ members/views.py
@@ -1,4 +1,4 @@
-﻿from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
