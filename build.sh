dd17f8949a82f332897f47d5b4049472e31c5bbe
zachariahbioto3-sketch <zachariahbioto3@gmail.com>
2026-09-10 10:33:40 +0300

    the foramtions on th bush scrippts

--- build.sh
+++ build.sh
@@ -1,5 +1,15 @@
 ﻿#!/usr/bin/env bash
 set -o errexit
 pip install -r requirements.txt
-python manage.py collectstatic --no-input
+python manage.py collectstatic --noinput
 python manage.py migrate
+python manage.py shell -c "
+from django.contrib.auth import get_user_model
+import os
+User = get_user_model()
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('admin', 'admin@kafuosa.com', os.environ.get('DJANGO_SUPERUSER_PASSWORD', 'changeme'))
+    print('Superuser created')
+else:
+    print('Superuser already exists')
+"
--- members/views.py
+++ members/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from .models import Member
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
