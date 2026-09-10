b60dc3132cd6a2f693b42fad0796e8204db52657
zachariahbioto3-sketch <zachariahbioto3@gmail.com>
2026-09-10 16:26:52 +0300

    fix: move staticfiles above cloudinary_storage to fix admin CSS on Render

--- hh/settings.py
+++ hh/settings.py
@@ -17,8 +17,8 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'cloudinary_storage',
     'django.contrib.staticfiles',
+    'cloudinary_storage',
     'cloudinary',
     'pages',
     'members',
@@ -93,6 +93,7 @@ CLOUDINARY_STORAGE = {
     'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
     'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
     'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
+    'STATICFILES': False,
 }
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
