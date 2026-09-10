6f05b1f447a8b9ab6532a87602eb7cd53b162d6b
zachariahbioto3-sketch <zachariahbioto3@gmail.com>
2026-09-10 13:09:40 +0300

    fix hero image paths reomve statics tags from inline style

---
+++ fix_revert.py
@@ -0,0 +1,13 @@
+﻿content = open('templates/pages/home.html', encoding='utf-8').read()
+content = content.replace(
+    "style=\"background-image:url('{% static \\'images/hero-group.jpg\\' %}'")\"",
+    "style=\"background-image:url('/static/images/hero-group.jpg')\""
+).replace(
+    "style=\"background-image:url('{% static \\'images/hero-outreach.jpg\\' %}'")\"",
+    "style=\"background-image:url('/static/images/hero-outreach.jpg')\""
+).replace(
+    "style=\"background-image:url('{% static \\'images/hero-clinic.jpg\\' %}'")\"",
+    "style=\"background-image:url('/static/images/hero-clinic.jpg')\""
+)
+open('templates/pages/home.html', 'w', encoding='utf-8').write(content)
+print('Done')
