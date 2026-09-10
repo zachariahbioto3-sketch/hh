8d3095e19b91eaa4642e0e6e692df9e14bc00ab0
zachariahbioto3-sketch <zachariahbioto3@gmail.com>
2026-09-10 12:28:23 +0300

    new logo added to the system

--- hh/settings.py
+++ hh/settings.py
@@ -83,6 +83,7 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
