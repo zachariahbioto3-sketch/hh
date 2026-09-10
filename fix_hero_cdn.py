c08fe43af6a02bcbd37cf48b11e872450cf72530
zachariahbioto3-sketch <zachariahbioto3@gmail.com>
2026-09-11 00:20:06 +0300

    the cloudinary logic restored

---
+++ fix_hero_cdn.py
@@ -0,0 +1,15 @@
+﻿with open('templates/pages/home.html', encoding='utf-8') as f:
+    content = f.read()
+content = content.replace(
+    "url('/static/images/hero-group.jpg')",
+    "url('PASTE_CLOUDINARY_URL_HERE')"
+).replace(
+    "url('/static/images/hero-outreach.jpg')",
+    "url('PASTE_CLOUDINARY_URL_HERE')"
+).replace(
+    "url('/static/images/hero-clinic.jpg')",
+    "url('PASTE_CLOUDINARY_URL_HERE')"
+)
+with open('templates/pages/home.html', 'w', encoding='utf-8') as f:
+    f.write(content)
+print('Done')
