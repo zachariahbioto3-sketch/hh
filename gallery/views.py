dae290adca1124083d383c2c4ff672a5e24e2c69
zachariahbioto3-sketch <zachariahbioto3@gmail.com>
2026-09-18 23:52:16 +0300

    fix: gallery templates split, correct related name, album covers, image fallbacks

--- gallery/views.py
+++ gallery/views.py
@@ -2,10 +2,9 @@ from django.shortcuts import render, get_object_or_404
 from .models import GalleryAlbum
 
 def gallery_list(request):
-    albums = GalleryAlbum.objects.all()
-    return render(request, "gallery/gallery_list.html", {"albums": albums})
+    albums = GalleryAlbum.objects.prefetch_related('photos').all()
+    return render(request, 'gallery/gallery_list.html', {'albums': albums})
 
 def gallery_detail(request, pk):
     album = get_object_or_404(GalleryAlbum, pk=pk)
-    return render(request, "gallery/album_detail.html", {"album": album})
-
+    return render(request, 'gallery/album_detail.html', {'album': album})
