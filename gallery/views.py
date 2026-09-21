ff27e2bce7f832a895bba5454ce9929ac3f68b23
zachariahbioto3-sketch <zachariahbioto3@gmail.com>
2026-09-21 17:00:35 +0300

    the new files has be taken

--- gallery/views.py
+++ gallery/views.py
@@ -1,10 +1,14 @@
-from django.shortcuts import render, get_object_or_404
+﻿from django.shortcuts import render, get_object_or_404
 from .models import GalleryAlbum
 
 def gallery_list(request):
     albums = GalleryAlbum.objects.prefetch_related('photos').all()
-    return render(request, 'gallery/gallery_list.html', {'albums': albums})
+    total_photos = sum(a.photos.count() for a in albums)
+    return render(request, 'gallery/gallery_list.html', {
+        'albums': albums,
+        'total_photos': total_photos,
+    })
 
-def gallery_detail(request, pk):
+def album_detail(request, pk):
     album = get_object_or_404(GalleryAlbum, pk=pk)
     return render(request, 'gallery/album_detail.html', {'album': album})
